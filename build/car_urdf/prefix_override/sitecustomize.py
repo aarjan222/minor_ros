929a9efc69887916a7cc508cfdb5e4432133cf3f
aarjanbu <077bei004.aarjan@pcampus.edu.np>
2024-02-04 12:22:17 +0545

    Remove build, log, and install folders from version control

--- build/car_urdf/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/aarjan/minor_ros/install/car_urdf'
--- build/pubsub/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/rpi/minor_ros/install/pubsub'
